09842c48c1440a6d409bcfb06116c4de9b0f1968
Amane345 <62469708+Amane345@users.noreply.github.com>
2020-11-29 14:29:50 -0500

    Add files via upload

---
+++ GetRates/GetRates/GetRate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetRates
+{
+   public class GetRate
+    {
+        public int ContractId { get; set; }
+        public decimal Price { get; set; }
+       public string ServiceType { get; set; }
+       public int TransitDay { get; set; }
+
+
+
+
+    }
+}
